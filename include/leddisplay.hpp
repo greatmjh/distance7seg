8222f325ee027242df93e8851f17e51bda815e24
greatmjh <66207287+greatmjh@users.noreply.github.com>
2022-04-20 20:31:29 +0200

    Add comments

--- include/leddisplay.hpp
+++ include/leddisplay.hpp
@@ -6,40 +6,40 @@
 class LEDdisplay {
 private:
 	//Internal module info
-	const unsigned int moduleCount;
-	unsigned int* moduleGrounds = nullptr;
+	const unsigned int moduleCount; //Number of modules being multiplexed by this object
+	unsigned int* moduleGrounds = nullptr; //Dynamic array of ground pins for multiplexing
 	
 	//Constants
-	const bool numbers[11][7] = {
-		{true, true, true, true, true, true, false},
-		{false, true, true, false, false, false, false},
-		{true, true, false, true, true, false, true},
-		{true, true, true, true, false, false, true},
-		{false, true, true, false, false, true, true},
-		{true, false, true, true, false, true, true},
-		{true, false, true, true, true, true, true},
-		{true, true, true, false, false, false, false},
-		{true, true, true, true, true, true, true},
-		{true, true, true, true, false, true, true},
-		{false, false, false, false, false, false, false}
+	const bool numbers[11][7] = { //The "font" to use to display things
+		{true, true, true, true, true, true, false}, 		//0
+		{false, true, true, false, false, false, false}, 	//1
+		{true, true, false, true, true, false, true}, 		//2
+		{true, true, true, true, false, false, true},		//3
+		{false, true, true, false, false, true, true},		//4
+		{true, false, true, true, false, true, true},		//5
+		{true, false, true, true, true, true, true},		//6
+		{true, true, true, false, false, false, false},		//7
+		{true, true, true, true, true, true, true},			//8
+		{true, true, true, true, false, true, true},		//9
+		{false, false, false, false, false, false, false}	//Blank
 	};
-	const unsigned int multiplexUpdateInterval = 2;
-	const unsigned int segmentPins[7];
+	const unsigned int multiplexUpdateInterval = 2; //How often to multiplex
+	const unsigned int segmentPins[7]; //Will get initialised in initialiser
 
 	//Globals
-	unsigned int currentlyDisplayingModule = 0;
-	unsigned int lastDisplayedDigit = ARRAY_LENGTH(numbers);
-	unsigned long lastMultiplexUpdate = 0;
-	unsigned int currentlyDisplayingNumber;
+	unsigned int currentlyDisplayingModule = 0; //Which module is being targeted by multiplexor
+	unsigned int lastDisplayedDigit = ARRAY_LENGTH(numbers); //What is currently out on the pins
+	unsigned long lastMultiplexUpdate = 0; //Last time multiplexing happened
+	unsigned int currentlyDisplayingNumber; //The number being displayed over the whole module
 
 	//Internal utility functions
-	void displayDigit(unsigned int digit);
-	void updateCurrentDisplay();
+	void displayDigit(unsigned int digit); //Push a digit to the pins
+	void updateCurrentDisplay(); //Push the current multiplexing info to ground pins
 
 public:
 	//Setup
 	LEDdisplay(unsigned int _moduleCount, unsigned int segmentA, unsigned int segmentB, unsigned int segmentC, unsigned int segmentD, unsigned int segmentE, unsigned int segmentF, unsigned int segmentG);
-	void addModule(size_t moduleID, unsigned int module);
+	void addModule(size_t moduleID, unsigned int module); //Initialiser won't add modules, so run this after
 	void multiplex(); //Run every loop cycle
 	//Setters
 	void setCurrentlyDisplayingNumber(unsigned int num);
--- src/main.cpp
+++ src/main.cpp
@@ -3,21 +3,26 @@
 #include "leddisplay.hpp"
 #include "ultrasonic.hpp"
 
-//Runtime globals
+//For non-blocking pulling from the ultrasonic sensor
 unsigned long lastUltrasonicUpdate = 0;
 const int ultrasonicUpdateInterval = 100;
+
+//Objects in use
 Ultrasonic* ultrasonic;
 LEDdisplay* display;
 
 void setup() {
+	//Initialise a display object that has the common pins and the number of displays
 	display = new LEDdisplay(2, PIN_7SEG_A, PIN_7SEG_B, PIN_7SEG_C, PIN_7SEG_D, PIN_7SEG_E, PIN_7SEG_F, PIN_7SEG_G);
+	//Add the display modules to the main object
 	display->addModule(0, PIN_7SEG_G1);
 	display->addModule(1, PIN_7SEG_G2);
+	//Initialise an ultrasonic sensor with its relevant pins
 	ultrasonic = new Ultrasonic(PIN_US_ECHO, PIN_US_TRIG);
 }
 
 void loop() {
-	display->multiplex();
+	display->multiplex(); //Multiplex the display
 	
 	//Read ultrasonic periodically
 	if (millis() - lastUltrasonicUpdate > ultrasonicUpdateInterval) { //Check if the time has come to check
