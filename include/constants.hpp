0e93dc6896c51ed4d29d79689403434fbe6c474e
greatmjh <66207287+greatmjh@users.noreply.github.com>
2022-04-20 15:22:19 +0200

    Change 6 to have a top

--- include/constants.hpp
+++ include/constants.hpp
@@ -23,7 +23,7 @@ const bool numbers[10][7] = {
 	{true, true, true, true, false, false, true},
 	{false, true, true, false, false, true, true},
 	{true, false, true, true, false, true, true},
-	{false, false, true, true, true, true, true},
+	{true, false, true, true, true, true, true},
 	{true, true, true, false, false, false, false},
 	{true, true, true, true, true, true, true},
 	{true, true, true, false, false, true, true}
